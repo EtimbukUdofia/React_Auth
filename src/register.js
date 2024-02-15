581241e4e4bacb79f64463619acbd962740e5f64
EtimbukUdofia <etimbukudofia9@gmail.com>
2024-02-15 11:17:31 +0100

    add font awesome packages
    Also add register component to app

---
+++ src/register.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { useRef, useState, useEffect } from 'react';
+
+const Register = () => {
+  return (
+    <div>Register</div>
+  )
+}
+
+export default Register;
