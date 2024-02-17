870c7b5f7963284d2caa1db9ec5d4c1cb4fb35f5
EtimbukUdofia <etimbukudofia9@gmail.com>
2024-02-17 08:27:28 +0100

    add simple logout functionality

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,22 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import useAuth from '../hooks/useAuth';
 
 const Home = () => {
+  const { auth, setAuth } = useAuth();
+  const navigate = useNavigate();
+
+  const logOut = async () => {
+    //axios to /logout endpoint
+    setAuth({});
+    navigate('/linkpage');
+  }
+
   return (
     <section>
       <h1>Home</h1>
       <br />
-      <p>You are logged in!</p>
+      <p>{auth.user} is logged in!</p>
       <br />
       <Link to="/editor">Go to the Editor page</Link>
       <br />
@@ -16,7 +26,7 @@ const Home = () => {
       <br />
       <Link to="/linkpage">Go to the link page</Link>
       <div className="flexGrow">
-        {/* <button onClick={logout}>Sign Out</button> */}
+        <button onClick={logOut}>Sign Out</button>
       </div>
     </section>
   );
